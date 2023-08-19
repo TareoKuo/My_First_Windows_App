313528b2cbd79f44298a9f52f9294d1bb28fe86c
TareoKuo <kty891027@gmail.com>
2023-08-20 00:53:48 +0800

    Add get UUID

--- My_first_Windows_App/MainWindow.xaml.cs
+++ My_first_Windows_App/MainWindow.xaml.cs
@@ -2,7 +2,6 @@
 using System.Management;
 using System.Windows;
 using System.Diagnostics;
-using System.ComponentModel;
 
 namespace My_first_Windows_App
 {
@@ -20,11 +19,8 @@ namespace My_first_Windows_App
         {
             try
             {
-                // 使用 ManagementObjectSearcher 來查詢 Win32_Processor 類的資訊
-                // 使用 foreach 遍歷查詢結果中的每個 ManagementObject
-                // 從 ManagementObject 中獲取處理器資訊（Name 屬性）
-                // 將處理器資訊顯示在 TextBox1 控制項中
-                ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_Processor");
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_Processor");
+                ManagementObjectSearcher searcher2 = new ManagementObjectSearcher("SELECT * FROM Win32_ComputerSystemProduct");
                 foreach (ManagementObject queryObj in searcher.Get())
                 {
                     string processorName = queryObj["Name"].ToString();
@@ -32,13 +28,19 @@ namespace My_first_Windows_App
                     TextBox1.Text = processorName;
                     TextBox2.Text = systemName;
                 }
+
+                foreach (ManagementObject queryObj in searcher2.Get())
+                {
+                    string uuid = queryObj["UUID"].ToString();
+                    TextBox3.Text = uuid;
+                    break;
+                }
             }
             catch (ManagementException ex)
             {
-                // 如果出現 ManagementException 錯誤，將 "ERROR" 文字顯示在 TextBox1 中
-                // 顯示一個彈出式視窗來顯示錯誤訊息
                 TextBox1.Text = "ERROR";
                 TextBox2.Text = "ERROR";
+                TextBox3.Text = "ERROR";
                 MessageBox.Show("An error occurred while querying for WMI data: " + ex.Message);
             }
         }
@@ -48,11 +50,10 @@ namespace My_first_Windows_App
             string websiteUrl = "https://github.com/TareoKuo/My_first_Windows_App/tree/master";
             try
             {
-                Process.Start(new ProcessStartInfo // 這是一個用於配置 Process 的初始化設定的新 ProcessStartInfo 物件
-                {
-                    FileName = websiteUrl,
-                    UseShellExecute = true // 是否使用操作系統的外殼來啟動程序
-                });
+                // 這是一個用於配置 Process 的初始化設定的新 ProcessStartInfo 物件
+                // UseShellExecute 是否使用操作系統的外殼來啟動程序
+                ProcessStartInfo startInfo = new ProcessStartInfo { FileName = websiteUrl, UseShellExecute = true };
+                Process.Start(startInfo);
             }
             catch (Exception ex)
             {
@@ -63,6 +64,7 @@ namespace My_first_Windows_App
         private void Menu_Refresh_click(object sender, RoutedEventArgs e)
         {
             DisplayHardwareInfo();
+            MessageBox.Show("Refresh finished");
         }
         private void Menu_Exit_click(object sender, RoutedEventArgs e)
         {
@@ -74,3 +76,4 @@ namespace My_first_Windows_App
         }
     }
 }
+
